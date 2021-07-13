2736cdcb4339bc0d0f3db4afd1316638ee442b6e
Berkhout <s.w.berkhout@uu.nl>
2021-07-13 15:40:03 +0200

    add state space and acf plot

--- R/DescriptivesTimeSeries.R
+++ R/DescriptivesTimeSeries.R
@@ -18,3 +18,104 @@
 DescriptivesTimeSeriesInternal <- function(jaspResults, dataset, options) {
     jaspDescriptives::.DescriptivesTimeSeries(jaspResults, dataset, options)
 }
+
+.tsStateSpacePlot <- function(jaspResults, dataset, options, ready) {
+  stateSpacePlot <- createJaspPlot(title = "State Space Plot")
+  stateSpacePlot$dependOn(c("dependentVariable", "stateSpacePlot"))
+
+  jaspResults[["stateSpacePlot"]] <- stateSpacePlot
+
+  if (!ready)
+    return()
+
+  .tsFillStateSpacePlot(stateSpacePlot, dataset, options)
+
+  return()
+}
+
+.tsFillStateSpacePlot <- function(stateSpacePlot, dataset, options) {
+  yName <- options$dependentVariable[1]
+  y     <- dataset[, yName]
+  yLag  <- c(NA, y[-length(y)])
+
+  dat <- data.frame(y, yLag)
+  dat <- na.omit(dat)
+
+  xBreaks <- jaspGraphs::getPrettyAxisBreaks(yLag)
+  yBreaks <- jaspGraphs::getPrettyAxisBreaks(y)
+
+  forceLinearSmooth <- options$regressionType == "linear"
+  # Does not work with bquote
+  # plot <- jaspGraphs::JASPScatterPlot(dat$yLag, dat$y,
+  #                                     xName = bquote(.(decodeColNames(yName))[t-1]), 
+  #                                     yName = bquote(.(decodeColNames(yName))[t]),
+  #                                     plotAbove = "none", plotRight = "none"
+  #                                     )
+
+  plot <- ggplot2::ggplot(dat, ggplot2::aes(x = yLag, y = y)) + jaspGraphs::geom_point() +
+    ggplot2::scale_x_continuous(name = bquote(.(decodeColNames(yName))[t-1]), 
+                                breaks = xBreaks, limits = range(xBreaks)) +
+    ggplot2::scale_y_continuous(name = bquote(.(decodeColNames(yName))[t]), 
+                                breaks = yBreaks, limits = range(yBreaks))
+
+  if(options$addSmooth){
+    plot <- plot + ggplot2::geom_smooth(se = options$addSmoothCI, level = options$addSmoothCIValue,
+                                        method = if (forceLinearSmooth) "lm" else "loess")
+  }
+  plot <- jaspGraphs::themeJasp(plot)
+
+  stateSpacePlot$plotObject <- plot
+  
+  return()
+}
+
+.tsACF <- function(jaspResults, dataset, options, ready){
+  acfPlot <- createJaspPlot(title = "Autocorrelation Function")
+  acfPlot$dependOn(c("dependentVariable", "acf"))
+
+  jaspResults[["acfPlot"]] <- acfPlot
+
+  if (!ready)
+    return()
+
+  .tsFillACF(acfPlot, dataset, options)
+
+  return()
+}
+
+.tsFillACF <- function(acfPlot, dataset, options) {
+  y <- dataset[, options$dependentVariable[1]]
+  y <- na.omit(y)
+
+  yACF <- acf(y, plot = F)
+
+  dat <- data.frame(ACF = yACF$acf, Lag = yACF$lag)
+
+  yRange <- dat$ACF
+
+  plot <- ggplot2::ggplot()
+  if(options$addLinesCI){
+    ci        <- options$addLinesCIValue
+    clim      <- qnorm((1 + ci)/2)/sqrt(yACF$n.used)
+    yRange    <- c(yRange, clim, -clim)
+    dfSegment <- data.frame(x = min(xBreaks), xend = max(xBreaks), y = c(clim, -clim))
+
+    plot <- plot +
+      ggplot2::geom_segment(ggplot2::aes(x = x, xend = xend, y = y, yend = y),
+                            linetype = "dashed", color = "blue", data = dfSegment)
+  }
+
+  xBreaks <- jaspGraphs::getPrettyAxisBreaks(dat$Lag)
+  yBreaks <- jaspGraphs::getPrettyAxisBreaks(yRange)
+
+  plot <- plot +
+    ggplot2::geom_linerange(data = dat, ggplot2::aes(x = Lag, ymin = 0, ymax = ACF)) +
+    ggplot2::scale_x_continuous(name = "Lag", breaks = xBreaks, limits = range(xBreaks)) +
+    ggplot2::scale_y_continuous(name = "ACF", breaks = yBreaks, limits = range(yBreaks))
+
+  plot <- jaspGraphs::themeJasp(plot)
+
+  acfPlot$plotObject <- plot
+  
+  return()
+}
